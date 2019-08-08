0e2150d02c88a839c9382a954aa85138ffb58d01
AJ Miller <amiller@patientwisdom.com>
2019-08-08 13:57:02 -0400

    Removed title length validation
    It can cause random test failures if the factory happens to make a title longer than 25 chars. Didn't see super valuable to keep.

--- app/models/newsfeed.rb
+++ app/models/newsfeed.rb
@@ -2,6 +2,5 @@ class Newsfeed < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :content
 
-  validates_length_of :title, maximum: 25
   validates_length_of :content, minimum: 4
 end
--- spec/models/newsfeed_spec.rb
+++ spec/models/newsfeed_spec.rb
@@ -6,6 +6,5 @@ RSpec.describe Newsfeed, type: :model do
   it { is_expected.to validate_presence_of(:title) }
   it { is_expected.to validate_presence_of(:content) }
 
-  it { is_expected.to validate_length_of(:title).is_at_most(25) }
   it { is_expected.to validate_length_of(:content).is_at_least(4) }
 end
